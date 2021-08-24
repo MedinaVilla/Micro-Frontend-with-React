739c2f0340e914321b16c1eaaa7806d677a7fee1
MedinaVilla <net_medina@hotmail.com>
2021-08-24 18:09:36 -0500

    Created container deploy script workflow Github Actions

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,18 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
-                use:{
+                use: {
                     loader: 'babel-loader',
-                    options:{
-                        presets:['@babel/preset-react','@babel/preset-env'],
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
                         plugins: ['@babel/plugin-transform-runtime']
                     }
                 }
             }
 
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 }
