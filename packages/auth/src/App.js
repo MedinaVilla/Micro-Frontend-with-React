699ced765896b5966f78ee65a62418f18fd59419
MedinaVilla <net_medina@hotmail.com>
2021-08-29 15:42:45 -0500

    Auth sub app added, basic authentication

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import {createGenerateClassName, StylesProvider} from '@material-ui/core/styles';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
 import SignIn from './components/Signin';
 import SignUp from './components/Signup';
@@ -9,13 +9,18 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'au'
 })
 
-export default ({history})=>{
+export default ({ history, onSignIn }) => {
+    console.log(onSignIn);
     return <div>
         <StylesProvider generateClassName={generateClassName}>
             <Router history={history}>
                 <Switch>
-                    <Route path="/auth/signin" component={SignIn} />
-                    <Route path="/auth/signup" component={SignUp} />
+                    <Route path="/auth/signin">
+                        <SignIn onSignIn={onSignIn} />
+                    </Route>
+                    <Route path="/auth/signup" >
+                        <SignUp onSignIn={onSignIn} />
+                    </Route>
                 </Switch>
             </Router>
         </StylesProvider>
