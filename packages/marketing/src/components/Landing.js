27b28ddca45e98c16799fce2be0a6264188928b6
MedinaVilla <net_medina@hotmail.com>
2021-08-29 14:54:52 -0500

    Making Auth microfronend and some routing fixing to initialEntries of memory routing of subapplications

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -63,9 +63,9 @@ const useStyles = makeStyles((theme) => ({
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function Album() {
+export default function Album(props) {
   const classes = useStyles();
-
+  console.log(props);
   return (
     <React.Fragment>
       <main>
